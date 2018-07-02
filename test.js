7350982802d46566701dbabc6c8d9f180d5b6006
Kassy 'Kas' Eric <kasomaeric8@gmail.com>
2018-07-02 08:40:34 +0100

    Add files via upload
    Realized I had left out some crucial responsive styling (navbar heading/mobile-view button) also the article optimization for mobile. Last but not least added the correct js files (in the correct way) and added responsive styling for content/sidebar sections

--- test.js
+++ test.js
@@ -1,4 +1,21 @@
 
 $(document).ready(function(){
 
+	$(".toggle-sidebar").click(function(){
+		if(!$("#sidebar").is(":visible")){
+			$("#content").css("width","70%");
+		}
+		$("#sidebar").animate({width:'toggle'},350,function(){
+		if($("#sidebar").is(":visible")){
+			$("#content").css("width","70%");
+		}else{
+			$("#content").css("width","100%");
+		}
+		});
+	});
+
+	$("#mobile").click(function(){
+		$("ul#menu").toggle();
+	});
+
 });
